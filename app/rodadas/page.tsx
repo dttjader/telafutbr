a37f34d26f043fe874221b92d52913662c2ed4aa
dttjader <daniel@tjader.com.br>
2026-08-28 18:01:16 -0300

    Add files via upload

---
+++ app/rodadas/page.tsx
@@ -0,0 +1,9 @@
+import { getPartidas, getTimes, getEstadios } from '@/lib/data';
+import { RodadasClient } from '@/components/RodadasClient';
+
+export const dynamic = 'force-dynamic';
+
+export default async function RodadasPage() {
+  const [partidas, times, estadios] = await Promise.all([getPartidas(), getTimes(), getEstadios()]);
+  return <RodadasClient partidas={partidas} times={times} estadios={estadios} />;
+}
